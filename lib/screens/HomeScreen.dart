e36641b7096334fc929f1cd56fa7b7ec77639217
seen2 <tiwarietc@gmail.com>
2021-06-23 19:27:52 +0530

    Basic Navigations

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -5,8 +5,55 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text("Welcom To Home Screen")),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+            backgroundColor: Colors.deepPurple,
+            title: Text("Home"),
+            bottom: TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.directions_car)),
+                Tab(icon: Icon(Icons.directions_transit)),
+                Tab(icon: Icon(Icons.directions_bike)),
+              ],
+            )),
+        drawer: Drawer(),
+        body: Container(
+          child: Center(child: Text("Welcome To Home Screen")),
+        ),
+        bottomNavigationBar: DefaultTabController(
+          length: 4,
+          child: Container(
+            color: Colors.deepPurple,
+            child: TabBar(
+              labelColor: Colors.white,
+              unselectedLabelColor: Colors.white70,
+              indicatorSize: TabBarIndicatorSize.tab,
+              indicatorPadding: EdgeInsets.all(5.0),
+              indicatorColor: Colors.blue,
+              tabs: [
+                Tab(
+                  text: "Transactions",
+                  icon: Icon(Icons.euro_symbol),
+                ),
+                Tab(
+                  text: "Bills",
+                  icon: Icon(Icons.assignment),
+                ),
+                Tab(
+                  text: "Balance",
+                  icon: Icon(Icons.account_balance_wallet),
+                ),
+                Tab(
+                  text: "Options",
+                  icon: Icon(Icons.settings),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
